0ddb3f9d742c9d66c73053559b81af36645b3139
Chris Kelly <chris@affinitylicensing.com>
2025-11-14 12:12:43 -0700

    GH Actions Deploy and Additional API Field documentation (#22)

--- scripts/deploy-branch.js
+++ scripts/deploy-branch.js
@@ -6,11 +6,11 @@ var path = require('path');
 set('-e');
 set('-v');
 
-var branch = process.env.TRAVIS_BRANCH && process.env.TRAVIS_BRANCH.toLowerCase();
+var branch = process.env.BRANCH_NAME && process.env.BRANCH_NAME.toLowerCase();
 if (branch && branch !== 'gh-pages') {
-  var branchPath = path.join('.tmp', 'preview', branch, '/');
-  mkdir('-p', branchPath);
-  exec('npm run swagger bundle -- -o ' + branchPath + 'swagger.json');
-  cp('web/index.html', branchPath);
-  exec('deploy-to-gh-pages --update .tmp');
+	var branchPath = path.join('.tmp', 'preview', branch, '/');
+	mkdir('-p', branchPath);
+	exec('npm run swagger bundle -- -o ' + branchPath + 'swagger.json');
+	cp('web/index.html', branchPath);
+	exec('deploy-to-gh-pages --update .tmp');
 }
